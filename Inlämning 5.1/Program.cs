1b33a69ed334e5dd2d080efb35d5a2e590853556
7R1XY <theo.broden@gmail.com>
2023-11-22 09:58:18 +0100

    Add project files.

---
+++ Inlämning 5.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Xml;
+
+namespace inlam_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I detta program ska du skriva in fem namn");
+            string[] namn = new string[5];
+
+            for (int i = 0; i < namn.Length; i++)
+            {
+                Console.WriteLine($"Skriv in ett namn");
+                namn[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Här är de namnen du skrev in");
+
+            foreach (string i in namn)
+            {
+                Console.WriteLine(i);
+            }
+            
+           
+            
+        }
+    }
+}
